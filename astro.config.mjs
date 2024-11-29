8193fc0d7ebc3ebf2ebe322d273fd231fdc6b1f9
Michael Smith <throwawayclover@gmail.com>
2024-11-29 15:23:58 -0500

    chore: add Tailwind to project (#1)
    * chore: add base tailwind config
    
    * chore: configure Prettier for Tailwind and Astro
    
    * refactor: update HeaderLink to use Tailwind
    
    * chore: update header component to use tailwind
    
    * chore: convert Footer to Tailwind
    
    * chore: update BlogPost to use Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-// @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://nachos.dev",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: 'ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      mono: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;',
+    },
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
