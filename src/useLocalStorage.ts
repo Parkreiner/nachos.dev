51ebdff50a711e673d74d4dd910ac805ce55d2cb
Michael Smith <throwawayclover@gmail.com>
2024-12-15 07:17:15 -0500

    refactor: update how localStorage is injected

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -20,11 +20,6 @@ type BaseUseLocalStorageOptions = Readonly<{
   key: string;
 
   /**
-   * A custom localStorage implementation. Mainly relevant for testing.
-   */
-  localStorage?: Storage;
-
-  /**
    * If a null value would be written to localStorage, instead remove the
    * key-value pair entirely.
    */
@@ -56,154 +51,6 @@ type UseLocalStorageResult<T extends JsonValue = JsonValue> = readonly [
   setStorageValue: SetLocalStorageValue<T>,
   latestError: Error | undefined,
 ];
-
-export function useLocalStorage<T extends JsonValue = JsonValue>(
-  options: UseLocalStorageOptionsWithFallback<T>,
-): UseLocalStorageResult<T>;
-export function useLocalStorage<T extends JsonValue = JsonValue>(
-  options: UseLocalStorageOptionsWithoutFallback,
-): UseLocalStorageResult<T | null>;
-export function useLocalStorage<T extends JsonValue = JsonValue>(
-  options:
-    | UseLocalStorageOptionsWithoutFallback
-    | UseLocalStorageOptionsWithFallback<T>,
-): UseLocalStorageResult<T | null> {
-  const {
-    key,
-    fallbackValue,
-    removeNullValues = true,
-    syncFallbackOnMount = false,
-    localStorage = window.localStorage,
-  } = options;
-
-  const [latestError, setLatestError] = useState<Error>();
-
-  // We actually need useCallback and not useEffectEvent here, because of how
-  // useSyncExternalStore works. Every time useSync gets a new memory reference
-  // for its subscription callback, it unsubscribes with the old callback and
-  // resubscribes with the new one. We want that behavior every time the storage
-  // key changes
-  const subscribeToLocalStorage = useCallback<ReactSubscriptionCallback>(
-    (notifyReact) => {
-      const onStorageUpdate = (event: StorageEvent) => {
-        // Browsers don't support granular storage subscriptions; you have to
-        // subscribe to all events, and then tease out the parts you care about
-        const canIgnore =
-          event.storageArea !== localStorage || event.key !== key;
-        if (canIgnore) {
-          return;
-        }
-
-        // Using slightly wonkier syntax to force type narrowing on the values
-        // so that the main check condition doesn't have to deal with nulls
-        if (event.oldValue === null || event.newValue === null) {
-          if (event.oldValue !== event.newValue) {
-            notifyReact();
-          }
-          return;
-        }
-
-        try {
-          const oldParsed = JSON.parse(event.oldValue);
-          const newParsed = JSON.parse(event.newValue);
-          if (!deepEqual(oldParsed, newParsed)) {
-            notifyReact();
-          }
-        } catch (err) {
-          setLatestError(err as Error);
-        }
-      };
-
-      window.addEventListener("storage", onStorageUpdate);
-      return () => window.removeEventListener("storage", onStorageUpdate);
-    },
-    [key, localStorage],
-  );
-
-  const readFromLocalStorage = (): T | null => {
-    const payload = localStorage.getItem(key);
-    if (payload === null) {
-      return null;
-    }
-
-    try {
-      const parsed = JSON.parse(payload) as T;
-      return parsed;
-    } catch {
-      /**
-       * @todo Figure out if there's a good way to surface these errors to the
-       * rest of the hook. The state getter must be pure, so you can't call
-       * state setters from here. Exposing the error as a value might require
-       * refactoring all the state to live outside React.
-       */
-      return null;
-    }
-  };
-
-  const storageState = useSyncExternalStore(
-    subscribeToLocalStorage,
-    readFromLocalStorage,
-  );
-
-  const hookValue =
-    storageState === null && fallbackValue !== undefined
-      ? fallbackValue
-      : storageState;
-
-  // Using useEffectEvent to make the state setter mirror the behavior from
-  // useState's state setter as closely as possible (namely, that the
-  // function maintains a stable reference for the entire duration of the
-  // component)
-  const setLocalStorageValue: SetLocalStorageValue<T | null> = useEffectEvent(
-    (payload) => {
-      let newValue: T | null;
-      if (typeof payload !== "function") {
-        newValue = payload;
-      } else {
-        try {
-          newValue = payload(hookValue);
-        } catch (err) {
-          setLatestError(err as Error);
-          return;
-        }
-      }
-
-      if (newValue === null && removeNullValues) {
-        localStorage.removeItem(key);
-        return;
-      }
-
-      try {
-        const string = JSON.stringify(newValue);
-        localStorage.setItem(key, string);
-      } catch (err) {
-        setLatestError(err as Error);
-      }
-    },
-  );
-
-  // It's generally a really bad idea to have an effect that only runs on mount
-  // without resolving the reactivity properly, but I didn't want to add the
-  // overhead of useEffectEvent for one-time logic
-  useEffect(() => {
-    const canSyncOnMount =
-      syncFallbackOnMount &&
-      storageState === null &&
-      fallbackValue !== undefined;
-    if (!canSyncOnMount) {
-      return;
-    }
-
-    try {
-      const string = JSON.stringify(fallbackValue);
-      localStorage.setItem(key, string);
-    } catch (err) {
-      setLatestError(err as Error);
-    }
-  }, []);
-
-  return [hookValue, setLocalStorageValue, latestError];
-}
 
 function deepEqual(v1: JsonValue, v2: JsonValue): boolean {
   // Not using Object.is here, because it's not that relevant for JSON, and it
@@ -262,3 +109,156 @@ function deepEqual(v1: JsonValue, v2: JsonValue): boolean {
 
   return true;
 }
+
+export function createUseLocalStorage(localStorage: Storage) {
+  function useLocalStorage<T extends JsonValue = JsonValue>(
+    options: UseLocalStorageOptionsWithFallback<T>,
+  ): UseLocalStorageResult<T>;
+  function useLocalStorage<T extends JsonValue = JsonValue>(
+    options: UseLocalStorageOptionsWithoutFallback,
+  ): UseLocalStorageResult<T | null>;
+  function useLocalStorage<T extends JsonValue = JsonValue>(
+    options:
+      | UseLocalStorageOptionsWithoutFallback
+      | UseLocalStorageOptionsWithFallback<T>,
+  ): UseLocalStorageResult<T | null> {
+    const {
+      key,
+      fallbackValue,
+      removeNullValues = true,
+      syncFallbackOnMount = false,
+    } = options;
+
+    const [latestError, setLatestError] = useState<Error>();
+
+    // We actually need useCallback and not useEffectEvent here, because of how
+    // useSyncExternalStore works. Every time useSync gets a new memory
+    // reference for its subscription callback, it unsubscribes with the old
+    // callback and resubscribes with the new one. We want that behavior every
+    // time the storage key changes
+    const subscribeToLocalStorage = useCallback<ReactSubscriptionCallback>(
+      (notifyReact) => {
+        const onStorageUpdate = (event: StorageEvent) => {
+          // Browsers don't support granular storage subscriptions; have to
+          // subscribe to all events, and then filter
+          const canIgnore =
+            event.storageArea !== localStorage || event.key !== key;
+          if (canIgnore) {
+            return;
+          }
+
+          // Using slightly wonkier syntax to force type narrowing on the values
+          // so that the main check condition doesn't have to deal with nulls
+          if (event.oldValue === null || event.newValue === null) {
+            if (event.oldValue !== event.newValue) {
+              notifyReact();
+            }
+            return;
+          }
+
+          try {
+            const oldParsed = JSON.parse(event.oldValue);
+            const newParsed = JSON.parse(event.newValue);
+            if (!deepEqual(oldParsed, newParsed)) {
+              notifyReact();
+            }
+          } catch (err) {
+            setLatestError(err as Error);
+          }
+        };
+
+        window.addEventListener("storage", onStorageUpdate);
+        return () => window.removeEventListener("storage", onStorageUpdate);
+      },
+      [key, localStorage],
+    );
+
+    const readFromLocalStorage = (): T | null => {
+      const payload = localStorage.getItem(key);
+      if (payload === null) {
+        return null;
+      }
+
+      try {
+        const parsed = JSON.parse(payload) as T;
+        return parsed;
+      } catch {
+        /**
+         * @todo Figure out if there's a good way to surface these errors to the
+         * rest of the hook. The state getter must be pure, so you can't call
+         * state setters from here. Exposing the error as a value might require
+         * refactoring all the state to live outside React.
+         */
+        return null;
+      }
+    };
+
+    const storageState = useSyncExternalStore(
+      subscribeToLocalStorage,
+      readFromLocalStorage,
+    );
+
+    const hookValue =
+      storageState === null && fallbackValue !== undefined
+        ? fallbackValue
+        : storageState;
+
+    // Using useEffectEvent to make the state setter mirror the behavior from
+    // useState's state setter as closely as possible (namely, that the
+    // function maintains a stable reference for the entire duration of the
+    // component)
+    const setLocalStorageValue: SetLocalStorageValue<T | null> = useEffectEvent(
+      (payload) => {
+        let newValue: T | null;
+        if (typeof payload !== "function") {
+          newValue = payload;
+        } else {
+          try {
+            newValue = payload(hookValue);
+          } catch (err) {
+            setLatestError(err as Error);
+            return;
+          }
+        }
+
+        if (newValue === null && removeNullValues) {
+          localStorage.removeItem(key);
+          return;
+        }
+
+        try {
+          const string = JSON.stringify(newValue);
+          localStorage.setItem(key, string);
+        } catch (err) {
+          setLatestError(err as Error);
+        }
+      },
+    );
+
+    // It's generally a really bad idea to have an effect that only runs on
+    // mount without resolving the reactivity properly, but I didn't want to add
+    // the overhead of useEffectEvent for one-time logic
+    useEffect(() => {
+      const canSyncOnMount =
+        syncFallbackOnMount &&
+        storageState === null &&
+        fallbackValue !== undefined;
+      if (!canSyncOnMount) {
+        return;
+      }
+
+      try {
+        const string = JSON.stringify(fallbackValue);
+        localStorage.setItem(key, string);
+      } catch (err) {
+        setLatestError(err as Error);
+      }
+    }, []);
+
+    return [hookValue, setLocalStorageValue, latestError];
+  }
+
+  return useLocalStorage;
+}
+
+export const useLocalStorage = createUseLocalStorage(window.localStorage);
